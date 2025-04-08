26865f4d0757185a06cbcf7a660194662fc0ab3b
Mahmoud-Ramadan2 <mahmoudramadan385@gmail.com>
2025-04-09 00:24:06 +0200

    imp userServices and gituserbyemail, imp Global ex handler to handle user not found

---
+++ src/main/java/com/example/contacatapi/contacatapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.example.contacatapi.contacatapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+public ResponseEntity<String> userNotFoundExceptionHandler(UserNotFoundException ex){
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+}
---
+++ src/main/java/com/example/contacatapi/contacatapi/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.contacatapi.contacatapi.service;
+
+import com.example.contacatapi.contacatapi.exception.UserNotFoundException;
+import com.example.contacatapi.contacatapi.model.User;
+import com.example.contacatapi.contacatapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+  private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User getUserByEmail(String email){
+
+        return userRepository.getUserByEmail(email).orElseThrow(()->new UserNotFoundException("user with Email: " + email +" does`t exist"));
+    }
+
+}
