413bc18255a61c6f7cadd4b6eb08c4b01dbb7524
Mahmoud-Ramadan2 <mahmoudramadan385@gmail.com>
2025-04-08 19:13:42 +0200

    initialization commit

---
+++ src/main/java/com/example/contacatapi/contacatapi/ContacatapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.contacatapi.contacatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContacatapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContacatapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/contacatapi/contacatapi/ContacatapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contacatapi.contacatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContacatapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
